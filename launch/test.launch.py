cd66deb42176774c5dd8a6c6762795570132b393
Etienne Schmitz <contact@etienne-schmitz.com>
2021-03-14 22:40:24 +0100

    nav work for one robot (without obstacle)

--- launch/test.launch.py
+++ launch/test.launch.py
@@ -16,8 +16,42 @@ def generate_launch_description():
 
     map_file = os.path.join(get_package_share_directory(
         'rostron_bringup'), 'config', 'map.yaml')
+    nav2_yaml = os.path.join(get_package_share_directory(
+        'rostron_bringup'), 'config', 'nav2_params.yaml')
+    default_bt_xml_path = os.path.join(get_package_share_directory(
+        'rostron_bringup'), 'config', 'navigate_w_replanning_and_recovery.xml')
 
     return LaunchDescription([
+        # Controller
+        Node(
+            package='nav2_controller',
+            node_executable='controller_server',
+            output='screen',
+            parameters=[nav2_yaml]),
+        # Planner
+        Node(
+            package='nav2_planner',
+            node_executable='planner_server',
+            node_name='planner_server',
+            output='screen',
+            parameters=[nav2_yaml]),
+
+        # Recoveries
+        Node(
+            package='nav2_recoveries',
+            node_executable='recoveries_server',
+            node_name='recoveries_server',
+            output='screen'),
+
+        # To see
+        Node(
+            package='nav2_bt_navigator',
+            node_executable='bt_navigator',
+            node_name='bt_navigator',
+            output='screen',
+            parameters=[nav2_yaml, {'bt_xml_filename': default_bt_xml_path}]),
+
+
         Node(
             package='nav2_map_server',
             node_executable='map_server',
@@ -39,9 +73,12 @@ def generate_launch_description():
         Node(
             package='nav2_lifecycle_manager',
             node_executable='lifecycle_manager',
-            name='lifecycle_manager_localization',
+            node_name='lifecycle_manager_navigation',
             output='screen',
-            parameters=[{'use_sim_time': True},
-                        {'autostart': True},
-                        {'node_names': ['map_server']}])
+            parameters=[{'autostart': True},
+                        {'node_names': ['controller_server',
+                                        'planner_server',
+                                        'recoveries_server',
+                                        'bt_navigator',
+                                        'map_server']}])
     ])
